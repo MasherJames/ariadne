b592fd93df4950362e1f5239005dacd2958a5e20
James Macharia <jamesmasher13@gmail.com>
2019-09-16 02:12:46 +0300

    Add apollo gateway

---
+++ apollo-gateway/gateway.js
@@ -0,0 +1,38 @@
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    {
+      name: "accounts",
+      url: "http://localhost:8000/graphql/"
+    },
+    {
+      name: "products",
+      url: "http://127.0.0.1:8001/graphql/"
+    },
+    {
+      name: "reviews",
+      url: "http://127.0.0.1:8002/graphql/"
+    }
+  ]
+});
+
+const init = async () => {
+  let gatewayConfig;
+
+  try {
+    gatewayConfig = await gateway.load();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+
+  const server = new ApolloServer({
+    ...gatewayConfig
+  });
+  server.listen({ port: 4000 }).then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  });
+};
+init();
